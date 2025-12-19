c2520302aba1bb3c9bd2e5316b3bb5e8866cb386
edlakin04 <edlakin2004@gmail.com>
2025-12-19 02:36:27 +0000

    Update db.js

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -4,13 +4,9 @@ let pool;
 function getPool() {
   if (pool) return pool;
 
-  if (!process.env.DATABASE_URL) {
-    throw new Error("Missing DATABASE_URL env var");
-  }
-
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : undefined,
   });
 
   return pool;

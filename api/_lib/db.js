669398f1207888e08c2485d72af1a70bd2ff6e2f
edlakin04 <edlakin2004@gmail.com>
2025-12-19 00:24:44 +0000

    Update db.js

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -1,1 +1,20 @@
+const { Pool } = require("pg");
 
+let pool;
+
+function getPool() {
+  if (pool) return pool;
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Missing DATABASE_URL env var");
+  }
+
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  return pool;
+}
+
+module.exports = { getPool };
